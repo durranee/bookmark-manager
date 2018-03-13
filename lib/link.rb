5701243170494d5418f1c755f4d05937d63718df
Noelle Legrain <noelle.legrain@gmail.com>
2018-03-13 11:54:18 +0000

    Updated gemfile with pg, updated .all method

--- lib/link.rb
+++ lib/link.rb
@@ -1,11 +1,15 @@
+require 'pg'
+
 class Link
 attr_reader :links
 
+def self.all
+  connection = PG.connect(dbname: 'bookmark_manager')
+  result = connection.exec("SELECT * FROM links")
+  result.map { |link| link['url'] }
+end
+
   def initialize
     @links = ["www.google.co.uk", "www.bbc.com", "www.cnn.com"]
-  end
-
-  def all
-    links
   end
 end
