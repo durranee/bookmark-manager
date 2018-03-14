743347eaec326d2c6603036b85f2164f96bf9fab
Noelle Legrain <noelle.legrain@gmail.com>
2018-03-14 19:03:22 +0000

    Added rack gem

--- spec/test_database_setup.rb
+++ spec/test_database_setup.rb
@@ -1,11 +1,12 @@
 require 'pg'
-require 'databaseconnection'
+# require 'databaseconnection'
 p "Setting up test database..."
 
-# connection = PG.connect(dbname: 'bookmark_manager_test')
+connection = PG.connect(dbname: 'bookmark_manager_test')
 
-DatabaseConnection.query("Truncate links;")
+connection.exec("Truncate links;")
 
-DatabaseConnection.query("INSERT INTO links VALUES(1, 'http://www.makersacademy.com');")
-DatabaseConnection.query("INSERT INTO links VALUES(2, 'http://www.google.com');")
-DatabaseConnection.query("INSERT INTO links VALUES(3, 'http://www.facebook.com');")
+connection.exec("INSERT INTO links VALUES(1, 'http://www.makersacademy.com');")
+connection.exec("INSERT INTO links VALUES(2, 'http://www.google.com');")
+connection.exec("INSERT INTO links VALUES(3, 'http://www.facebook.com');")
+connection.exec("INSERT INTO links VALUES(4, 'http://www.gfhgfgh.com');")
